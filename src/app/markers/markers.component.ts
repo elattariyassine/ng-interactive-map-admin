6725aab72e8937fb12232275e961a8d39c86a6a4
El attari Yassine <yassineattari64@gmail.com>
2020-02-11 13:13:42 +0100

    [First] Initial commit & Added core comonent & marker form

---
+++ src/app/markers/markers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-markers',
+  templateUrl: './markers.component.html',
+  styleUrls: ['./markers.component.scss']
+})
+export class MarkersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
